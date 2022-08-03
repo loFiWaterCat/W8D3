853d5bb5c74fcf065f9990c7a83eac91b77e840b
mattzteh <mattzteh@gmail.com>
2022-08-03 11:47:19 -0700

    finish piece

--- reversi/src/piece.js
+++ reversi/src/piece.js
@@ -2,18 +2,24 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    if (this.color === 'white'){
+        return 'black';
+    };
+    return 'white';
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    return this.color = this.oppColor();
 };
 
 /**
@@ -21,7 +27,8 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
-};
+    return this.color.charAt(0).toUpperCase();
+}
 
 // DON'T TOUCH THIS CODE
 if (typeof window === 'undefined'){
